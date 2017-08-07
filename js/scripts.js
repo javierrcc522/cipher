3749d57b16e7425e3764cf21e2acfd7815e67415
Javier Jimenez and John Parsons <javier@>
2017-08-07 15:23:03 -0700

    adds prompt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-// $(document).ready(function() {
-//   $("h1").click(function() {
-//     alert("This is a header.");
-//   });
-//   });
 var text = prompt("Write a sentence");
   console.log(text);
 
 
-var firstlast = function(sentence){
+var firstlast = function(sentence)
+  {
     var first= sentence.charAt(0);
     var lastChar = sentence.length-1;
     var last = sentence.charAt(lastChar);
 
-  return first+last;
-}
+    return first+last;
+  }
+
 
 var twoLetters = firstlast(text);
+
 console.log(twoLetters);
+
+alert("The first and last letters of what you wrote are: " + twoLetters);
